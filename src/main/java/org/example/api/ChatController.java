3f45646ea6ebc0674ecdaf2f6a269ab7c2679a95
wangshaocong <shaocong.wang@dbappsecurity.com.cn>
2026-03-09 20:34:06 +0800

    feat: 调用模型test

--- src/main/java/org/example/api/ChatController.java
+++ src/main/java/org/example/api/ChatController.java
@@ -1,24 +1,34 @@
 package org.example.api;
-
+import lombok.RequiredArgsConstructor;
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 
 @RestController
+@RequestMapping("/ai")
+@RequiredArgsConstructor(onConstructor_ = {@Autowired})
 public class ChatController {
 
     private final ChatClient chatClient;
 
-    public ChatController(ChatClient.Builder chatClientBuilder) {
-        this.chatClient = chatClientBuilder.build();
-    }
-
     @GetMapping("/api/chat")
-    public String chat(@RequestParam String message) {
+    public String chat(@RequestParam String prompt) {
         return chatClient.prompt()
-                .user(message)
+                .user(prompt)
                 .call()
                 .content();
     }
+
+    @GetMapping(value = "/api/chat/stream", produces = "text/html;charset=utf-8")
+    public Flux<String> chatStream(@RequestParam String prompt) {
+        return chatClient.prompt()
+                .user(prompt)
+                .stream()
+                .content();
+    }
+
 }
---
+++ src/main/java/org/example/config/ClientConfig.java
@@ -0,0 +1,20 @@
+package org.example.config;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.ollama.OllamaChatModel;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ClientConfig {
+
+    @Bean
+    @ConditionalOnMissingBean
+    public ChatClient chatClient(OllamaChatModel model){
+        return ChatClient.builder(model)
+                .defaultSystem("你是一名专业的机器人助手，名字叫团团，你需要专业且礼貌的回答用户的问题。")
+                .build();
+    }
+
+}
